4e26641d839a7dc6a55996ac5033620dea48744b
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-27 09:55:58 -0400

    new files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Jumbotron from "./components/jumbotron";
+import Wrapper from "./components/wrapper";
+import Card from "./components/friendCard";
+import friends from "./friends.json";
+import "./style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  state = {
+    friends,
+    score: 0,
+    highScore: 0,
+  };
+
+
+  gameOver = () => {
+    if (this.state.score > this.state.highScore) {
+      this.setState({highScore: this.state.score}, function() {
+        console.log(this.state.highScore);
+      });
+    }
+    this.state.friends.forEach(friend => {
+      friend.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.friends.find((y, i) => {
+      if (y.id === id) {
+        if(friends[i].count === 0){
+          friends[i].count = friends[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.friends.sort(() => Math.random() - 1)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+  render() {
+  return(
+    <Wrapper>
+  <Jumbotron score={this.state.score} highScore={this.state.highScore}>Clicky Game</Jumbotron>
+{
+ 
+  this.state.friends.map( card => (
+    <Card
+      clickCount={this.clickCount}
+      id={card.id}
+      key={card.id}
+      image={card.image}
+    />
+  ))
+}
+  </Wrapper >
+  
   );
 }
-
+}
 export default App;
