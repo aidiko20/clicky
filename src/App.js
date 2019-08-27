78d3f68e9e6d06e607831c1634878b427e4f5169
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-27 11:07:49 -0400

    fixed images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Jumbotron from "./components/jumbotron";
 import Wrapper from "./components/wrapper";
 import Card from "./components/friendCard";
